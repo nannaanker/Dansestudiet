db63feb329d85df9acbb9d3ab708ee0614a467f5
Nanna <naho0001@stud.kea.dk>
2024-05-01 09:12:26 +0200

    hh

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -19,6 +19,23 @@ module.exports = {
                 'display': ['"Roboto"', 'monospace'], // Adds a new `font-display` class
                 
                 },
+
+                spacing: {
+                    '450': '450px',
+                    '260': '260px',
+                    '500': '500px',
+                    '600': '600px',
+                    '560': '560px',
+                    '580': '580px',
+                    '410': '481px',
+                    '700': '700px',
+                    '900': '900px',
+                    '100px': '100px',
+                    '390': '390px',
+                    '270': '270px',
+                    '110': '110px',
+                    
+                  },
                 
 
             colors: {
@@ -46,6 +63,8 @@ module.exports = {
                     500:'#231F20',
                   
                   },
+
+    
 
 
             }
